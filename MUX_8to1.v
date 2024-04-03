8f1fff80cec47f8d9b9de459996684c3f4bf15c8
ycchu8992 <65800463+ycchu8992@users.noreply.github.com>
2024-04-03 22:48:26 +0800

    mux complete but not yet test

--- MUX_8to1.v
+++ MUX_8to1.v
@@ -22,11 +22,21 @@
 //////////////////////////////////////////////////////////////////////////////////
 module MUX_8to1(a,b,c,d,e,f,g,h,sel,mux_out);
 input a,b,c,d,e,f,g,h;
-input [3:0] sel;	//from the output of scrambler
+input [2:0] sel;	//from the output of scrambler
 output reg mux_out;
 
-***************
-**Your Coding**
-***************
+    always @(*) begin
+        case (sel)
+            3'b000: mux_out = a; 
+            3'b001: mux_out = b; 
+            3'b010: mux_out = c; 
+            3'b011: mux_out = d; 
+            3'b100: mux_out = e; 
+            3'b101: mux_out = f; 
+            3'b110: mux_out = g; 
+            3'b111: mux_out = h; 
+            default: mux_out = a; 
+        endcase
+    end
 
 endmodule
