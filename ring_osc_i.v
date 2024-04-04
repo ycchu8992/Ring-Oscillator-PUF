9903f33783a61fd9b05bdc96ce0d4b7dc8e9887c
ycchu8992 <65800463+ycchu8992@users.noreply.github.com>
2024-04-04 12:19:20 +0800

    update

--- ring_osc_i.v
+++ ring_osc_i.v
@@ -20,91 +20,24 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
-module inverter_with_delay(in, out);
-input wire in;
-output wire out;
-
-    reg temp;
-
-    assign out = temp;
-
-   always @(*) begin
-        temp = ~in;
-    end
-    
-endmodule
-module ring_osc_i(clk, enable, rst, out);
+module Ring_Oscillator(clk, enable, rst, out);
 input clk, enable, rst;
 output out;
-
-    wire i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13;
-    reg next_i1, next_i2, next_i3, next_i4, next_i5, next_i6, next_i7;
-    reg next_i8, next_i9, next_i10, next_i11, next_i12, next_i13;
-    reg next_out;
+    reg [12:0] connect;
+    reg [12:0] next_connect;
 
     always @(posedge clk) begin
         if(rst) begin
-            next_out <= 1'b0;
-            next_i1 <= 1'b0;
-            next_i2 <= 1'b0;
-            next_i3 <= 1'b0;
-            next_i4 <= 1'b0;
-            next_i5 <= 1'b0;
-            next_i6 <= 1'b0;
-            next_i7 <= 1'b0;
-            next_i8 <= 1'b0;
-            next_i9 <= 1'b0;
-            next_i10 <= 1'b0;
-            next_i11 <= 1'b0;
-            next_i12 <= 1'b0;
-            next_i13 <= 1'b0;
+            next_connect <= 13'b1010_1010_1010_1;
         end else begin
-            next_out <= ~(enable & out);
-            next_i1 <= i1;
-            next_i2 <= i2;
-            next_i3 <= i3;
-            next_i4 <= i4;
-            next_i5 <= i5;
-            next_i6 <= i6;
-            next_i7 <= i7;
-            next_i8 <= i8;
-            next_i9 <= i9;
-            next_i10 <= i10;
-            next_i11 <= i11;
-            next_i12 <= i12;
-            next_i13 <= i13;
+            next_connect <= connect;
         end       
-    end    
+    end
     
-    inverter_with_delay inverter_un(.in(next_out), .out(i1));
-    
-    inverter_with_delay inverter_deux(.in(next_i1), .out(i2));
-    
-    inverter_with_delay inverter_trois(.in(next_i2), .out(i3));
-    
-    inverter_with_delay inverter_quatre(.in(next_i3), .out(i4));
-    
-    inverter_with_delay inverter_cinq(.in(next_i4), .out(i5));
-    
-    inverter_with_delay inverter_six(.in(next_i5), .out(i6));
-    
-    inverter_with_delay inverter_sept(.in(next_i6), .out(i7));
-    
-    inverter_with_delay inverter_huit(.in(next_i7), .out(i8));
-    
-    inverter_with_delay inverter_neuf(.in(next_i8), .out(i9));
-    
-    inverter_with_delay inverter_dix(.in(next_i9), .out(i10));
-    
-    inverter_with_delay inverter_onze(.in(next_i10), .out(i11));
-    
-    inverter_with_delay inverter_douze(.in(next_i11), .out(i12));
-    
-    inverter_with_delay inverter_treize(.in(next_i12), .out(i13));
-    
-    inverter_with_delay inverter_quatorze(.in(next_i13), .out(out));
-    
+    assign out =  connect[12];
+
+    always @(*) begin
+        connect[12:0] = { ~next_connect[11:0], ~(enable & out)};
+    end
+
 endmodule
-
-
--- ring_osc_t.v
+++ ring_osc_t.v
@@ -6,35 +6,39 @@ module ring_osc_t ();
 
     parameter DELAY = 2;
 
-    wire out;
+    wire out_0, out_1, out_2, out_3, out_4, out_5, out_6, out_7;
     reg clk = 1'b1;
-    reg rst = 1'b0;
+    reg [7:0] rst = 8'b0;
     reg enable = 1'b1;
 
     always #1 clk = ~clk;
 
     //====================================
     // TODO: Connect your module here. Please connect it by port name but not order
-    ring_osc_i ring_osc(.clk(clk), .enable(enable), .rst(rst), .out(out));
+    Ring_Oscillator ro_0(.clk(clk), .enable(enable), .rst(rst[0]), .out(out_0));
+    Ring_Oscillator ro_1(.clk(clk), .enable(enable), .rst(rst[1]), .out(out_1));
+    Ring_Oscillator ro_2(.clk(clk), .enable(enable), .rst(rst[2]), .out(out_2));
+    Ring_Oscillator ro_3(.clk(clk), .enable(enable), .rst(rst[3]), .out(out_3));
+    Ring_Oscillator ro_4(.clk(clk), .enable(enable), .rst(rst[4]), .out(out_4));
+    Ring_Oscillator ro_5(.clk(clk), .enable(enable), .rst(rst[5]), .out(out_5));
+    Ring_Oscillator ro_6(.clk(clk), .enable(enable), .rst(rst[6]), .out(out_6));
+    Ring_Oscillator ro_7(.clk(clk), .enable(enable), .rst(rst[7]), .out(out_7));
     //====================================
 
-    integer i, error_count;
+    integer i;
     initial begin
 
         $display("===== Simulation ======");
 
-        error_count = 0;
         #DELAY;
-
-        rst = 1'b1;
+        rst = 8'b1000_0000;
 
         #DELAY;
-
-        rst = 1'b0;
+        rst = rst >> 1;
 
         for(i = 0 ; i < 50 ; i = i + 1) begin
             #DELAY;
-             $display("out = %b", out);
+            rst = rst >> 1;
         end
 
         $finish;
